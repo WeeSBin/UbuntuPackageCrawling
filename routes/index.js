5e331105c0a3e9fca7c9e98741a022d52d825795
git5620 <git1234>
2021-02-18 18:01:37 +0900

    패키지 다운로드 테스트 완료
    next 각 패키지 파일링

--- routes/index.js
+++ routes/index.js
@@ -1,34 +1,56 @@
 const express = require('express');
 const axios = require('axios');
 const cheerio = require('cheerio');
+const open = require('open');
 const log = console.log;
 
 const router = express.Router();
 
-const getHtml = async () => {
+
+const downloadedPackage= [];
+
+const getHtml = (url) => {
   try {
-    return await axios.get('https://packages.ubuntu.com/groovy/alien');
+    return axios.get('https://packages.ubuntu.com' + url);
   } catch (e) {
     console.error(e);
   }
 };
 
-getHtml()
-    .then(html => {
-      let depends = [];
-      const $ = cheerio.load(html.data);
-      const dependsList = $('div#pdeps').children('ul.uldep').children('li');
-
-      dependsList.each(function (index, element) {
-        depends.push($(element).find('a').text())
+const downloadPackage = ($) => {
+    const $download = $('div#pdownload th a');
+    getHtml($download.attr('href'))
+      .then(html => {
+          const $ = cheerio.load(html.data);
+          const url = $('div.cardleft li a').attr('href');
+            log(url)
+            open(url);
       })
+};
 
-      log(depends);
-    })
+const getPackage = (url) => {
+    getHtml(url)
+        .then(html => {
+          let depends = [];
+          const $ = cheerio.load(html.data);
+          downloadPackage($);
+          const $dependsList = $('div#pdeps').children('ul.uldep').children('li');
+          $dependsList.each(function (index, element) {
+              if ($(element).find('a').text().startsWith('libc6')) {
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+              } else {
+                  const href = $(element).find('a').attr('href');
+                  if (!downloadedPackage.includes(href)) {
+                    downloadedPackage.push(href)
+                      depends.push(href);
+                      getPackage(href)
+                  }
+              }
+          });
+          // log(depends);
+        })
+};
+
+getPackage('/groovy/rpm-common');
 
 module.exports = router;
